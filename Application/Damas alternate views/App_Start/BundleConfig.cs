5969e070a7af9f823308b447c615d2ec6360495a
Jan Veselý <6-166-1@unicornuniverse.eu>
2016-01-15 09:41:54 +0100

    ADV controler initialized with views

--- Application/Damas alternate views/App_Start/BundleConfig.cs
+++ Application/Damas alternate views/App_Start/BundleConfig.cs
@@ -7,7 +7,9 @@ namespace Damas_alternate_views
     {
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
+        
         {
+            BundleTable.EnableOptimizations = false;
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -26,6 +28,64 @@ namespace Damas_alternate_views
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/Scripts/ADV2/js").Include(
+                      "~/Content/ADV/worldLow.js"));
+            bundles.Add(new StyleBundle("~/Content/ADV/css").Include(
+                      "~/Content/ADV/bootstrap.min.css",
+                      "~/Content/ADV/bootstrap-switch.min.css",
+                      "~/Content/ADV/components.min.css",
+                      "~/Content/ADV/custom.min.css",
+                      "~/Content/ADV/daterangepicker.min.css",
+                      "~/Content/ADV/font-awesome.min.css",
+                      "~/Content/ADV/fullcalendar.min.css",
+                      "~/Content/ADV/jqvmap.css",
+                      "~/Content/ADV/layout.min.css",
+                      "~/Content/ADV/morris.css",
+                      "~/Content/ADV/plugins.min.css",
+                      "~/Content/ADV/simple-line-icons.min.css",
+                      "~/Content/ADV/uniform.default.css"));
+            bundles.Add(new ScriptBundle("~/Scripts/ADV/js").Include(
+                      "~/Content/ADV/amcharts.js",
+                      "~/Content/ADV/ammap.js",
+                      "~/Content/ADV/amstock.js",
+                      "~/Content/ADV/app.min.js",
+                      "~/Content/ADV/bootstrap-hover-dropdown.min.js",
+                      "~/Content/ADV/bootstrap-switch.min.js",
+                      "~/Content/ADV/bootstrap.min.js",
+                      "~/Content/ADV/chalk.js",
+                      "~/Content/ADV/dashboard.min.js",
+                      "~/Content/ADV/daterangepicker.min.js",
+                      "~/Content/ADV/fullcalendar.min.js",
+                      "~/Content/ADV/jquery.blockui.min.js",
+                      "~/Content/ADV/jquery.counterup.min.js",
+                      "~/Content/ADV/jquery.easypiechart.min.js",
+                      "~/Content/ADV/jquery.flot.categories.min.js",
+                      "~/Content/ADV/jquery.flot.min.js",
+                      "~/Content/ADV/jquery.flot.resize.min.js",
+                      "~/Content/ADV/jquery.min.js",
+                      "~/Content/ADV/jquery.slimscroll.min.js",
+                      "~/Content/ADV/jquery.sparkline.min.js",
+                      "~/Content/ADV/jquery.uniform.min.js",
+                      "~/Content/ADV/jquery.vmap.europe.js",
+                      "~/Content/ADV/jquery.vmap.germany.js",
+                      "~/Content/ADV/jquery.vmap.js",
+                      "~/Content/ADV/jquery.vmap.russia.js",
+                      "~/Content/ADV/jquery.vmap.sampledata.js",
+                      "~/Content/ADV/jquery.vmap.usa.js",
+                      "~/Content/ADV/jquery.vmap.world.js",
+                      "~/Content/ADV/jquery.waypoints.min.js",
+                      "~/Content/ADV/js.cookie.min.js",
+                      "~/Content/ADV/layout.min.js",
+                      "~/Content/ADV/light.js",
+                      "~/Content/ADV/moment.min.js",
+                      "~/Content/ADV/morris.min.js",
+                      "~/Content/ADV/patterns.js",
+                      "~/Content/ADV/pie.js",
+                      "~/Content/ADV/quick-sidebar.min.js",
+                      "~/Content/ADV/radar.js",
+                      "~/Content/ADV/raphael-min.js",
+                      "~/Content/ADV/serial.js",
+                      "~/Content/ADV/worldLow.js"));
         }
     }
 }
---
+++ Application/Damas alternate views/Controllers/ADVController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Damas_alternate_views.Controllers
+{
+    public class ADVController : Controller
+    {
+        // GET: ADV
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
